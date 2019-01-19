c2a7e3eadb333bb66e211bad2ac2d0ce004d06a1
gsim2345 <>
2019-01-19 16:25:34 +0100

    added first example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Stephanie", age: 26}
+    ], 
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: 28},
+        {name: "Manu", age: 29},
+        {name: "Stephanie", age: 27}
+      ]});
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: "Max", age: 28},
+        {name: event.target.value, age: 29},
+        {name: "Stephanie", age: 26}
+      ]});
+  }
+
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid teal",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, New App!</h1>
+        <button style={style}
+          onClick={() => this.switchNameHandler("Maximilian!!")}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}>
+        </Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>
+        </Person>
       </div>
     );
   }
