4c235d1d51478dc890aefe5bb1f8b96deb099dc0
gsim2345 <>
2019-01-28 11:28:14 +0100

    Added UPDATE lifecycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -24,6 +24,33 @@ class Persons extends Component {
   componentDidMount() {
     console.log('[Persons.js] Inside componentDidMount()');
   }
+
+  // UPDATE LIFECYCLE
+  // update triggered by outside, by props change
+  componentWillReceiveProps(nextProps) {
+      console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
+  }
+
+  // UPDATE LIFECYCLE
+  // return true: updating continue, return false, updating stops
+  // worth to turn a component to a Class and add these lifecycle hooks, if for example receives a lot of props, and you only want to rerender if one of the props is changing.
+  shouldComponentUpdate(nextProps, nextState) {
+      console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+      // only updates if the new props is different then the original
+      return nextProps.persons !== this.props.persons;
+  }
+
+  // UPDATE LIFECYCLE
+  componentWillUpdate(nextProps, nextState) {
+      console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  // UPDATE LIFECYCLE
+  // we have no nextProps, nextState, as it is after the update. Use this.props, this.state
+  componentDidUpdate() {
+    console.log('[UPDATE Persons.js] Inside componentDidUpdate');
+  }
+
   
 // Component lyfecycle hooks:
   // 3.
